f7d233a68b2052082e13b4507f20bb0921ae8c49
ewgenius <ewgeniux@gmail.com>
2016-01-19 17:53:38 +0300

    root component done

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,28 @@ var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('webpack', () => {
-  gulp
-    .src('src/app/app.jsx')
-    .pipe(webpack({
-      module: {
-        loaders: [{
-          test: /\.jsx$/,
-          exlude: /node_modules/,
-          loader: 'babel-loader'
-        }]
-      },
-      output: {
-        filename: 'bundle.js'
-      }
-    }, null, err => console.log(err)))
-    .pipe(gulp.dest('dist/'));
-});
+gulp.task('webpack', () => gulp
+  .src('src/app/app.jsx')
+  .pipe(webpack({
+    module: {
+      loaders: [{
+        test: /\.jsx?$/,
+        exlude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }]
+    },
+    output: {
+      filename: 'bundle.js'
+    }
+  }, null, err => console.log(err)))
+  .pipe(gulp.dest('dist/js')));
 
-gulp.task('html', () => {});
+gulp.task('html', () => gulp.src('src/index.html').pipe(gulp.dest('dist/')));
+
+gulp.task('watch', () => {
+  gulp.watch(['src/**/*.js', 'src/**/*.jsx'], ['webpack']);
+  gulp.watch('src/index.html', ['html']);
+});
--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,1 +1,12 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>test</div>
+    );
+  }
+}
+
+ReactDom.render(<App/>, document.getElementById('main'));
